3403809db92b97d05bf31401dabeb26c6588f7cf
Prabhudatta Barik <thexas21@gmail.com>
2023-12-03 14:05:24 +0530

    Added new import images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,5 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
-import G from '../assets/G.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
@@ -19,13 +18,11 @@ import award03 from '../assets/award03.png';
 import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
-import gericht from '../assets/gericht.png';
 import epicurean from '../assets/epicurean.png';
 
 export default {
   bg,
   chef,
-  G,
   gallery01,
   gallery02,
   gallery03,
@@ -44,6 +41,5 @@ export default {
   award05,
   sign,
   quote,
-  gericht,
   epicurean,
 };
